6fd0a9804bdcb08c6ae071d528aae5522bf7e84c
AMIT ROUT <120554331+AmitRout1704@users.noreply.github.com>
2023-09-26 01:23:45 +0530

    voice recognization with care of noice detection and response being send to backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,36 @@
-const express=require('express')
-const app=express()
-app.listen(4000,()=>{
-    console.log('Server Running On Port 3000')
-})
+const express = require("express");
+const app = express();
+const port = 4000; // Change as needed
+const speech = require("node-speech-to-text");
+
+const cors = require("cors"); // Import the cors package
+
+app.use(express.json());
+
+// Configure CORS to allow requests from http://localhost:3000
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+    credentials: true,
+  })
+);
+
+app.post("/process-voice", async (req, res) => {
+  try {
+    const { voiceData } = req.body;
+
+    console.log(voiceData);
+    res.status(200).json({
+      sucess: "true",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
